45e59fe44968d21a5474449b6f657c115b6c6100
alexandercurtis <alex.curtis14@gmail.com>
2024-03-26 08:19:36 -0400

    more hashing practice

---
+++ src/CountSequence.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class CountSequence {
+    public CountSequence() {
+    }
+
+    public int CountElements(int [] arr) {
+        Set<Integer> nums = new HashSet<>();
+        int ans = 0;
+
+        for(int num: arr) {
+            nums.add(num);
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            if(nums.contains(i) && nums.contains(i+1)) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,9 @@ public class Main {
         MissingNumber numChecker = new MissingNumber();
         System.out.println(numChecker.CheckNums(new int[]{0, 1, 3}));
         System.out.println(numChecker.CheckNums(new int[]{0, 1, 2}));
+
+        CountSequence sequenceChecker = new CountSequence();
+        System.out.println(sequenceChecker.CountElements(new int[] {1, 2, 3, 4}));
+        System.out.println(sequenceChecker.CountElements(new int[] {1, 1, 4, 4}));
     }
 }
